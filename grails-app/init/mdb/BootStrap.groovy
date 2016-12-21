08863906bf2422c47fcab83e5b190f9833dd7b4d
Lokesh Devnani <lokeshthegenius@gmail.com>
2016-12-21 14:57:04 +0530

    bootstrap file updated

--- grails-app/init/mdb/BootStrap.groovy
+++ grails-app/init/mdb/BootStrap.groovy
@@ -6,36 +6,36 @@ class BootStrap {
         Movies movies = new Movies(
             title: "Interstellar",
             releaseYear : 2014,
-            summary : "relativity",
-            genre : [ name : "sci-fi" ],
+            summary : "In the future, Earth is slowly becoming uninhabitable. Ex-NASA pilot Cooper, along with a team of researchers, is sent on a planet exploration mission to report which planet can sustain life.",
+            genre : [ id: 18, name : "Sci-Fi" ],
             director : [ name: "Christopher Nolan" ]
         ).save();
         movies = new Movies(
                 title: "Inception",
                 releaseYear : 2010,
                 summary : "Cobb steals information from his targets by entering their dreams. He is wanted for his alleged role in his wife's murder and his only chance at redemption is to perform the impossible, an inception.",
-                genre : [ name : "sci-fi" ],
+                genre : [ id: 18, name : "Sci-Fi" ],
                 director : [ name: "Christopher Nolan" ]
         ).save();
         movies = new Movies(
                 title: "Shawshank Redemption",
                 releaseYear : 1994,
                 summary : "Andy Dufresne, a successful banker, is arrested for the murders of his wife and her lover, and is sentenced to life imprisonment at the Shawshank prison. He becomes the most unconventional prisoner.",
-                genre : [ name : "Crime" ],
+                genre : [ id: 5, name : "Crime" ],
                 director : [ name: "Frank Darabont" ]
         ).save();
         movies = new Movies(
                 title: "Shooter",
                 releaseYear : 2007,
                 summary : "Bob Lee Swagger, one of the world's greatest marksmen, is recruited by a colonel to protect the president, but finds himself on the run when he is framed for the president's murder.",
-                genre : [ name : "Thriller" ],
+                genre : [ id: 19, name : "Thriller" ],
                 director : [ name: " Antoine Fuqua" ]
         ).save();
         movies = new Movies(
                 title: "The Departed",
                 releaseYear : 2006,
                 summary : "An undercover agent and an espionage agent try to counter-attack each other in order to save themselves from being exposed in front of the authorities.",
-                genre : [ name : "Crime" ],
+                genre : [ id: 5, name : "Crime" ],
                 director : [ name: "Martin Scorsese" ]
         ).save();
 
