5727fc7884aa617b1188eb44a617dfc35f8be730
Lokesh Devnani <lokeshthegenius@gmail.com>
2017-02-04 14:29:08 +0530

    person userId added to Movies domain classes

--- grails-app/domain/mdb/Movies.groovy
+++ grails-app/domain/mdb/Movies.groovy
@@ -4,11 +4,24 @@ import grails.rest.*
 
 @Resource(uri = '/api/movies', readOnly = false, formats = ['json', 'xml'])
 class Movies {
+    def springSecurityService
+
+    String userId
     String title
     int releaseYear
     String summary
     Genres genre
     Directors director
+
+//   String getUserId() {
+//        return 1
+//        return springSecurityService.principal?.username
+//   }
+
+    def beforeInsert() {
+        userId =  springSecurityService.principal?.username
+    }
+
 
 //    static hasMany = [genre: Genres]
 //    static hasOne = [director: Directors]
